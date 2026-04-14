b1d97cd250e30fb8a75d3697ad5134eec7cb3b8f
RofikaMaulidiaPutri <Poponjanwars01gmail.com>
2026-04-14 07:54:46 +0700

    update v1

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+echo "Hello World";
+$nama = "PonzyBoy";
+$umur = 18;
+$tinggi = 167.9;
+$hobi = ["gamers","olaraga"," dan berdoa"];
+echo "Nama saya $nama, Umur saya $umur, Tinggi saya $tinggi, Hobi saya $hobi[0], $hobi[1], $hobi[2]"
+?>
